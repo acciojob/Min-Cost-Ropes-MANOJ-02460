f66b9bb24a601ca4ee0ad194cc76d894e0495cdc
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-06-15 19:19:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+	let minCost = 0;
+
+	while(arr.length>1){
+
+		arr.sort();
+
+		let first = arr.shift();
+
+		let second = arr.shift();
+
+		let newRope = first+second;
+
+		mincost+= newRope;
+
+		arr.push(newRope)
+
+		
+	}
+
+	return mincost
   
 }
 
